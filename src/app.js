01034cd669f883efebadc99d1e401779cf081bed
Annie Bae <bae.annie@gmail.com>
2017-01-19 16:40:59 -0500

    Map Components created to import in App

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Map from './Components/Map'
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				This is the REACT APP!
+				<Map />
 			</div>
 		)
 	}
