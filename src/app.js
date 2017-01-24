4d51d1245a9f43dc08cf84e18e650b05bc639608
Annie Bae <bae.annie@gmail.com>
2017-01-24 14:58:21 -0500

    making a call to Foursquare API

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Map from './components/Map'
 import Places from './components/Places'
+import superagent from 'superagent'
 
 class App extends Component {
 	componentDidMount() {
 		console.log('componentDidMount')
+
+		const url = 'https://api.foursquare.com/v2/venues/search?v=20140806&ll=40.7575285,-73.9884469&client_id=KPIGMRA32L30INFRQVANJDJHGQHJJ1XWGRFJDCZ2DNR4V3CS&client_secret=A0L1FCJXMICJEPHOF2AIXCUMUZFQZEFOI5MJEAT05ZYBIRHT'
+
+		superagent
+		.get(url)
+		.query(null)
+		.set('Accept', 'text/json')
+		.end((error, response) => {
+
+			console.log(JSON.stringify(response.body))
+
+		})
 	}
 
 	render() {
@@ -19,12 +32,6 @@ class App extends Component {
 				location: {
 					lat: 40.7575285,
 					lng: -73.9884469
-				}
-			},
-			{
-				location: {
-					lat: 40.7484405,
-					lng: -73.9878531
 				}
 			}
 		]
